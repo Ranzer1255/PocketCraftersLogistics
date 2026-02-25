1b703f56bae18765bac514ba03461e7fb797bf31
Ranzer <sgmaniac1255@gmail.com>
2026-02-25 12:56:18 -0600

    balance: now requires the Logistic robotics technology to be unlocked

--- data.lua
+++ data.lua
@@ -7,8 +7,10 @@ data:extend({
     icon_size = 64,
     small_icon = "__core__/graphics/icons/technology/effect/effect-logistic-slots.png",
     small_icon_size = 64,
-    
-    localised_name = "Request items to Craft Held item",
+    technology_to_unlock = "logistic-robotics",
+    unavailable_until_unlocked = true,    
+    localised_name = "Handcraft Request",
+    localized_description = "Hold an item or ghost in your hand and click here to request the materials to craft it from the Logistics Network",
     associated_control_input = "pc-request-hotkey"
   }
 })
